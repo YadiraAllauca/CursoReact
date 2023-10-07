ecca60127143f99087da036b0f5849c1051bf224
YadiraAllauca <94416577+YadiraAllauca@users.noreply.github.com>
2023-10-07 18:21:58 -0500

    Programación funcional

--- proyecto_curso/src/index.js
+++ proyecto_curso/src/index.js
@@ -1,3 +1,4 @@
+/*
 //Creación de objetos, funciones y arreglos
 const persona = {
   nombre: "Yadi",
@@ -9,4 +10,43 @@ persona.hablar();
 persona['nombre'] = "Carolina";
 
 const nombreobjetivo = "nombre";
-persona[nombreobjetivo.value] = "Raquel";
+persona[nombreobjetivo.value] = "Raquel"; 
+
+var log = function(mensaje){
+  console.log(mensaje);
+}
+
+var log2 = mensaje => {
+  console.log(mensaje)
+}
+
+const mensajes = [
+  "Pueden ser insertados en arreglos",
+  log,
+  log("hola"),
+  log2
+];
+
+const dentroFn = ingreso => {
+  ingreso("Pueden ser enviados como argumentos a otras funciones")
+}
+
+
+log("Hola")
+log2("Hola2")
+
+mensajes[1](mensajes[0]);
+mensajes[3](mensajes[2]);
+
+dentroFn(mensaje => console.log(mensaje));*/
+
+const crearMayus = function (registro) {
+  return function (mensaje) {
+    registro(mensaje.toUpperCase()+"!!!");
+  }
+}
+
+const enfasis = crearMayus(mensaje => console.log(mensaje));
+
+enfasis("La vida es mejor con música")
+enfasis("Me siento bien ahora")
