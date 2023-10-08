99c7684be8444128bbe723d2fd9b42dc5f2f21f5
YadiraAllauca <94416577+YadiraAllauca@users.noreply.github.com>
2023-10-07 21:30:55 -0500

    Estados y renderizado

--- proyecto_curso/src/componentes/contador.jsx
+++ proyecto_curso/src/componentes/contador.jsx
@@ -1,15 +1,30 @@
-/*import React, { Component } from "react";
-
+import React, { Component } from "react";
+/*
 class Contador extends Component {
 
   render() {
     return(<h1>Hola!</h1>)
   }
 }
-export default Contador*/
+export default Contador
 import React from "react";
 
 function Contador() {
+
     return (<h1>Hola!!</h1>);
+
+}
+export default Contador;*/
+
+class Contador extends Component {
+  state = {
+    count: 0
+  };
+  render() {
+    return (<div>
+      <span>{ this.state.count}</span>
+      <button>Incremento</button>
+    </div>);
+  }
 }
 export default Contador;
